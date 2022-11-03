76d7626a61a616cccebd56d8283dd75270b85667
hangYq <fellingyh@gmail.com>
2022-11-03 14:17:44 +0800

    feat: inital

---
+++ bin/camus_iconfont.dart
@@ -0,0 +1,5 @@
+import 'package:camus_iconfont/camus_iconfont.dart' as camus_iconfont;
+
+void main(List<String> arguments) {
+  print('Hello world: ${camus_iconfont.calculate()}!');
+}
---
+++ test/camus_iconfont_test.dart
@@ -0,0 +1,8 @@
+import 'package:camus_iconfont/camus_iconfont.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
